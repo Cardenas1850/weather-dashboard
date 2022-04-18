ce17e35a4043dc1defb0f7a4c0439c596a7479a2
Jason Cardenas <Jason.Cardenas@ftcsc.org>
2022-04-18 09:55:28 -0400

    pseudo code

---
+++ assets/js/script.js
@@ -0,0 +1,43 @@
+// variables
+
+//DOM variables
+
+//API key
+
+// function that will allow a user to search for the weather forecast
+
+// function that will fetch API key and url add 'units=imperial'
+
+// variables for the dates
+
+// create date on HTML
+
+// begin weather data below
+
+//weather icon image
+
+// use fetch to collect specific data values via innerHTML: wind speed, temperature, humidity, uv index
+
+/*
+current.temp
+current.humidity
+current.uvi
+current.wind-speed
+current.weather-icon
+current.weather-description
+*/
+
+// function to make color for UV
+
+// function to make 5 day forecast 
+
+// create cards via bootstrap for the five day forecast
+
+//Elements needed for 5 day forecast: temp windspeed humidity
+
+//function to make local storage, store serached cities
+
+// add funciton to delete local storage
+
+
+// call final fucntion
